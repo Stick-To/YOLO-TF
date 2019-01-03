4335b83ab8c0fd18b5be49e45ddbe03d48f56c13
xinru <34546552+Stick-To@users.noreply.github.com>
2019-01-03 20:06:36 +0800

    update

--- testYOLOv2.py
+++ testYOLOv2.py
@@ -6,8 +6,9 @@ import YOLOv2 as yolov2
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io, transform
+from utils.classname_encoder import classname_to_ids
 
-
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 device_name = tf.test.gpu_device_name()
 if device_name is not '':
@@ -15,13 +16,13 @@ if device_name is not '':
 else:
     print('Found GPU Device Failed!')
 
-lr = 0.001
+lr = 0.0001
 batch_size = 24
-buffer_size = 5011
-epochs = 160
-reduce_lr_epoch = []
+buffer_size = 256
+epochs = 280
+reduce_lr_epoch = [120,250]
 config = {
-    'mode': 'test',     # 'train', 'test'
+    'mode': 'train',     # 'train', 'test'
     'is_pretraining': False,
     'data_shape': [448, 448, 3],
     'num_classes': 20,
@@ -34,7 +35,7 @@ config = {
     'obj_scale': 5.,
     'class_scale': 1.,
 
-    'nms_score_threshold': 0.5,
+    'nms_score_threshold': 0.2,
     'nms_max_boxes': 10,
     'nms_iou_threshold': 0.5,
 
@@ -49,20 +50,26 @@ image_preprocess_config = {
     'is_random_crop': False,
     'random_horizontal_flip': 0.5,
     'random_vertical_flip': 0.,
-    'pad_truth_to': 50
+    'pad_truth_to': 60
 }
 
-data = ['./data/test_00000-of-00005.tfrecord',
-        './data/test_00001-of-00005.tfrecord',
-        './data/test_00002-of-00005.tfrecord',
-        './data/test_00003-of-00005.tfrecord',
-        './data/test_00004-of-00005.tfrecord']
+data = ['./data/test_00000-of-00010.tfrecord',
+        './data/test_00001-of-00010.tfrecord',
+        './data/test_00002-of-00010.tfrecord',
+        './data/test_00003-of-00010.tfrecord',
+        './data/test_00004-of-00010.tfrecord',
+        './data/test_00005-of-00010.tfrecord',
+        './data/test_00006-of-00010.tfrecord',
+        './data/test_00007-of-00010.tfrecord',
+        './data/test_00008-of-00010.tfrecord',
+        './data/test_00009-of-00010.tfrecord']
+
 
 train_gen = voc_utils.get_generator(data,
                                     batch_size, buffer_size, image_preprocess_config)
 trainset_provider = {
     'data_shape': [448, 448, 3],
-    'num_train': 5011,
+    'num_train': 22136,
     'num_val': 0,
     'train_generator': train_gen,
     'val_generator': None
@@ -71,7 +78,7 @@ trainset_provider = {
 
 
 testnet = yolov2.YOLOv2(config, trainset_provider)
-# testnet.load_weight()
+testnet.load_weight()
 for i in range(epochs):
     print('-'*25, 'epoch', i, '-'*25)
     if i in reduce_lr_epoch:
@@ -79,11 +86,14 @@ for i in range(epochs):
         print('reduce lr, lr=', lr, 'now')
     mean_loss = testnet.train_one_epoch(lr)
     print('>> mean loss', mean_loss)
-    testnet.save_weight('latest', './weight/test')
-# img = io.imread()
+    testnet.save_weight('>> latest', './weight/test')
+# img = io.imread('/home/test/Desktop/YOLO-TF-master/VOC/JPEGImages/000012.jpg')
 # img = transform.resize(img, [448,448])
 # img = np.expand_dims(img, 0)
 # result = testnet.test_one_image(img)
+
+
+# id_to_clasname = {k:v  for (v,k) in classname_to_ids.items()}
 # scores = result[0]
 # bbox = result[1]
 # class_id = result[2]
@@ -92,6 +102,7 @@ for i in range(epochs):
 # plt.imshow(np.squeeze(img))
 # axis = plt.gca()
 # for i in range(len(scores)):
-#     rect = patches.Rectangle((bbox[i][0],bbox[i][1]), bbox[i][2]-bbox[i][0],bbox[i][3]-bbox[i][1],linewidth=2,edgecolor='b',facecolor='none')
+#     rect = patches.Rectangle((bbox[i][1],bbox[i][0]), bbox[i][3]-bbox[i][1],bbox[i][2]-bbox[i][0],linewidth=2,edgecolor='b',facecolor='none')
 #     axis.add_patch(rect)
+#     plt.text(bbox[i][1],bbox[i][0], id_to_clasname[class_id[0]]+str(' ')+str(scores[0]), color='red')
 # plt.show()
